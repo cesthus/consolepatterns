edb34db18eaced898166b928514bed07a31bd680
cesthus <poveda3th@gmail.com>
2021-01-12 20:44:08 -0500

    Mediator pattern - parte 1

---
+++ Mediator/ChatRoom/AbstractChatroom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.ChatRoom
+{
+    /// <summary>
+    /// Mediator abstrac
+    /// </summary>
+    public abstract class AbstractChatroom
+    {
+        public abstract void register(Participant participant);
+        public abstract void register(string from, string to, string messafe);
+    }
+}
