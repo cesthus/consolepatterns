b3bbc4691ed576b1b941674933977bf7791a80b1
cesthus <poveda3th@gmail.com>
2021-01-26 17:54:18 -0500

    Flyweight pattern; shapes

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flyweight.Shapes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace Flyweight
     {
         static void Main(string[] args)
         {
+            ShapeObjectFactory sof = new ShapeObjectFactory();
+            IShape shape = sof.GetShape("Triangle");
+            shape.Print();
+            shape = sof.GetShape("Triangle");
+            shape.Print();
+            shape = sof.GetShape("Triangle");
+            shape.Print();
+
+            int total = sof.TotalObjectsCreated;
+            Console.WriteLine("Number of objects created: "+total);
+            Console.ReadKey();
         }
     }
 }
---
+++ Flyweight/Shapes/ShapeObjectFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight.Shapes
+{
+    public class ShapeObjectFactory
+    {
+        Dictionary<string, IShape> shapes = new Dictionary<string, IShape>();
+
+        public int TotalObjectsCreated
+        {
+            get { return shapes.Count; }
+        }
+
+        public IShape GetShape(string ShapeName)
+        {
+            IShape shape = null;
+            if (shapes.ContainsKey(ShapeName))
+            {
+                shape = shapes[ShapeName];
+            }
+            else
+            {
+                switch (ShapeName)
+                {
+                    case "Triangle":
+                        shape = new Triangle();
+                        shapes.Add("Triangle", shape);
+                        break;
+                    case "Square":
+                        shape = new Triangle();
+                        shapes.Add("Square", shape);
+                        break;
+                    default:
+                        throw new Exception("The factory cannot create the object");
+                }
+            }
+            return shape;
+        }
+
+    }
+}
